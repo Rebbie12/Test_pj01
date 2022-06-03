84e491d8422e46e9668d045d860797c8be3324de
Rebbie <qkong126@gmail.com>
2022-06-02 23:32:19 -0400

    fiist change

---
+++ test.py
@@ -0,0 +1,8 @@
+import requests
+city = 'london'
+api_url = 'https://api.api-ninjas.com/v1/airquality?city={}'.format(city)
+response = requests.get(api_url, headers={'X-Api-Key': 'xVYFBBO9TV/1vcuKc6QLWQ==i9Uv9nArjjslCtWc'})
+if response.status_code == requests.codes.ok:
+    print(response.text)
+else:
+    print("Error:", response.status_code, response.text)
